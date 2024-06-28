778ddcb35c14d1a5d411e4e3e5f7cae5f86f68df
samitkumarpatel-work <SamitKumar.Patel@maersk.com>
2024-06-28 16:55:17 +0200

    first commit

---
+++ src/main/java/net/samitkumar/voting_result_api/VotingResultApiApplication.java
@@ -0,0 +1,13 @@
+package net.samitkumar.voting_result_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingResultApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingResultApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/samitkumar/voting_result_api/VotingResultApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.samitkumar.voting_result_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingResultApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
